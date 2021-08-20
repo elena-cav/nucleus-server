4297f66a2e8758cf3cae234a69bb6700dd067c1c
Elena Cavallero <elenacavallero@outlook.com>
2021-08-20 12:24:34 +0100

    update pw

--- config/database.js
+++ config/database.js
@@ -1,15 +1,36 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "mongoose",
+//       settings: {
+//         uri: env("DATABASE_URI"),
+//         srv: env.bool("DATABASE_SRV", true),
+//         port: env.int("DATABASE_PORT", 27017),
+//         database: env("DATABASE_NAME"),
+//         username: env("DATABASE_USERNAME"),
+//         password: env("DATABASE_PASSWORD"),
+//       },
+//       options: {
+//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+//         ssl: env.bool("DATABASE_SSL", true),
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        uri: env("DATABASE_URI"),
+        host: env("DATABASE_HOST", "cluster0.bvzuv.mongodb.net"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
+        database: env("DATABASE_NAME", "nucleus-server"),
+        username: env("DATABASE_USERNAME", "3MXfBEsSwkZ2vZ"),
+        password: env("DATABASE_PASSWORD", "dJtlhBaKlv9AqOIM"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
