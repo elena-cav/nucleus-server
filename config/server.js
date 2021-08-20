9e8022e7f90a497023c1a1af4d06ec1fbd3e11eb
Elena Cavallero <elenacavallero@outlook.com>
2021-08-20 11:05:31 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e4ea88ffa78a9fcffae446dece8ed99d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '649113b4-8c44-4cf4-8cef-a4bad48dc89c'
+};
